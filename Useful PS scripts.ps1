319d8b99bd8e09c6e7e1ea80821f8725b2781904
Igor Olkhovskiy <legendarry1337@gmail.com>
2021-10-14 00:33:53 +0300

    Added an example of using the Robocopy

--- Useful PS scripts.ps1
+++ Useful PS scripts.ps1
@@ -15,6 +15,7 @@ $PSVersionTable
 # or
 powercfg /batteryreport /output "C:\battery-report.html"
 
+
 # run via CMD
 # build sln
 msbuild C:\Projects\ProjectName\SolutionName.sln /p:DebugSymbols=false /p:DebugType=None /p:RunCodeAnalysis=false /p:AllowedReferenceRelatedFileExtensions=none /p:DeployOnBuild=true /p:PublishProfile=Setup /flp:verbosity="diagnostic" /p:PublishBasePath="D\Test"
@@ -22,3 +23,20 @@ msbuild C:\Projects\ProjectName\SolutionName.sln /p:DebugSymbols=false /p:DebugT
 msbuild SolutionName.sln /flp:verbosity="diagnostic" /p:TrackFileAccess=false /p:DebugSymbols=false /p:DebugType=None /p:DeployOnBuild=true /p:PublishProfile=Setup /p:RunCodeAnalysis=false /p:AllowedReferenceRelatedFileExtensions=none
 
 msbuild /flp:verbosity="diagnostic" /p:TrackFileAccess=false /p:DebugSymbols=false /p:DebugType=None /p:DeployOnBuild=true /p:PublishProfile=Deploy /p:RunCodeAnalysis=false /p:AllowedReferenceRelatedFileExtensions=none
+
+
+# Example of using the Robocopy
+$source = 'D:\MyStorage';
+$destination = '\\UNC path\My remote storage';
+
+# these flags can also be added: /NJH (no job header), /NJS (no job summary)
+robocopy $source $destination /E /XC /XN /XO /NFL /NDL /NS /NC /LOG+:Log.log /TEE /R:3 /W:5
+# another example
+#robocopy $source $destination /E /XO /NDL /NS /NJH /UNILOG+:Log.log /TEE /R:3 /W:5 
+
+# Any value greater than 7 indicates that there was at least one failure during the copy operation.
+if ($lastexitcode -gt 7) {
+	write-host "Robocopy failed with exit code:" $lastexitcode
+} else {
+	write-host "Robocopy succeeded! Exit code:" $lastexitcode
+}
