3a27df6720b40af67abbbbf66ca66bca554759d2
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2022-09-26 01:11:27 +0300

    Added a Regex examples

---
+++ Projects/RegexExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexExample
+{
+	class Program
+	{
+		public static bool IsEmail(string s)
+		{
+			Regex regex = new Regex(@"^\w+([-.]\w+)*@\w+\.\w+([-.]\w+)*$");
+			return regex.IsMatch(s);
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(IsEmail("www.aa_bb.a@gmail.com.ua")); // true
+			Console.WriteLine(IsEmail("www_a-a.a@gmail.com"));		// true
+			Console.WriteLine(IsEmail("www_a-a.agmail.com.ua"));	// false
+			Console.WriteLine(IsEmail("a-a.a@gmail"));				// false
+
+			// replace symbols that don't match the pattern
+			string input = "12xy34";
+			string pattern = "[^xy]";
+			var regex = new Regex(pattern);
+
+			Console.WriteLine(regex.Replace(input, "+")); // output: ++xy++
+		}
+	}
+}
