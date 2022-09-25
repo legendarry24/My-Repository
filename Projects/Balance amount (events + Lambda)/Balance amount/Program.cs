befb14f12cc415f68b1fb970b3b360f9cafdc600
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2022-09-25 20:02:15 +0300

    Added another example of using events

---
+++ Projects/Balance amount (events + Lambda)/Balance amount/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Balance_amount
+{
+	public delegate void MyEventHandler();
+
+	internal static class Bank
+	{
+		private static decimal _balance;
+
+		public static decimal Balance
+		{
+			get => _balance;
+			set
+			{
+				_balance = value;
+				BalanceChanged?.Invoke();   // invoke event when private fields value changes
+			}
+		}
+		public static event MyEventHandler BalanceChanged;
+	}
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			// use an anonymous delegate as an event handler
+			Bank.BalanceChanged += delegate // without () cuz no parameters
+			{
+				Console.WriteLine($"The balance amount is {Bank.Balance}");
+			};
+			// use a lambda function as an event handler
+			Bank.BalanceChanged += () =>
+			{
+				if (Bank.Balance > 500)
+					Console.WriteLine($"You reached your savings goal! You have {Bank.Balance}");
+			};
+
+			while (true)
+			{
+				Console.WriteLine("How much to deposit?");
+				string input = Console.ReadLine();
+				if (input == "exit")
+					break;
+				Bank.Balance += Convert.ToDecimal(input);
+			}
+		}
+	}
+}
---
+++ Projects/Basic + Chained Events + Lambda/BasicEvents/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace BasicEvents
+{
+	// define the delegate for the event handler
+	public delegate void MyEventHandler(string value);
+
+	internal class EventPublisher
+	{
+		private string _str;
+
+		// declare the event
+		public event MyEventHandler ValueChanged; // Action<string> can be used instead of MyEventHandler
+		// .Net event handler
+		public event EventHandler<ObjChangeEventArgs> ObjChanged;
+
+		public string Str
+		{
+			set
+			{
+				_str = value;
+				// when the value changes, invoke the events
+				ValueChanged?.Invoke(_str);
+				ObjChanged?.Invoke(this, new ObjChangeEventArgs { PropChanged = "another event", NewValue = value });
+			}
+		}
+	}
+
+	internal class ObjChangeEventArgs // : EventArgs (explicit inheritance is not necessary if name ends with EventArgs)
+	{
+		public string PropChanged;
+		public string NewValue;
+	}
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var publisher = new EventPublisher();
+			// use a named function as an event handler
+			publisher.ValueChanged += ValueChangedHandler;
+			// Use a Lambda expression to define an event handler
+			publisher.ValueChanged += value => Console.WriteLine($"The value changed to {value} one more time");
+			// Note that this is a statement lambda, due to use of { }
+			publisher.ValueChanged += x => { Console.WriteLine($"The value changed to {x} (Lambda)"); };
+			// use an anonymous delegate as an event handler
+			publisher.ObjChanged += delegate (object sender, ObjChangeEventArgs e)
+			{
+				Console.WriteLine($"{sender.GetType()} had the '{e.PropChanged}' invoked with value '{e.NewValue}'");
+			};
+
+			string str;
+			do
+			{
+				Console.WriteLine("Enter a value: ");
+				str = Console.ReadLine();
+				publisher.Str = str;
+			} while (!str.Equals("exit"));
+		}
+
+		private static void ValueChangedHandler(string value)
+		{
+			Console.WriteLine($"The value changed to {value}");
+		}
+	}
+}
